3d01833c163d84d13a4f2834db7c8f5b2873798c
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-02 18:45:52 +0800

    Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-   <div><h1>Hello World</h1></div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route expect path="/" component={Home} />
+        <Route expect path="/login" component={Login} />
+        <Route expect path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
