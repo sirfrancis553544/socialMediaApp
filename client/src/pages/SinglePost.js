c365e25e6852f92d6f8c7d4e3ca3484c6b804dd3
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-13 23:44:02 +0800

    v

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { Card, Grid } from "semantic-ui-react";
+import { Card, Grid, Image } from "semantic-ui-react";
 import moment from "moment";
+import LikeButton from "../components/LikeButton";
+import { AuthContext } from "../context/auth";
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
-
+  const { user } = useContext(AuthContext);
   const {
     data: { getPost },
   } = useQuery(FETCH_POST_QUERY, {
@@ -46,9 +48,23 @@ function SinglePost(props) {
                 <Card.Header>{username}</Card.Header>
                 <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
                 <Card.Description>{body}</Card.Description>
-                        </Card.Content>
-                        <hr />
-                        
+              </Card.Content>
+              <hr />
+              <Card.Content>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log("Comment on post")}
+                >
+                  <Button basic color="blue">
+                    <Icon name="comments" />
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
+                </Button>
+              </Card.Content>
             </Card>
           </Grid.Column>
         </Grid.Row>
